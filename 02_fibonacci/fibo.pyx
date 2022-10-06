4db623ce6ef1a2f1fd19373624c39f65f7259e8e
e-hkr <meteorology522@gmail.com>
2022-10-06 13:42:48 +0900

    change dirname

---
+++ 02_fibonacci/fibo.pyx
@@ -0,0 +1,2 @@
+def fibo(n):
+  return n if n <= 1 else fibo(n-1) + fibo(n-2)
---
+++ 03_file_io/read_binary_cy.pyx
@@ -0,0 +1,6 @@
+import numpy as np
+
+def read_binary_cy(skip=0):
+  data = np.fromfile('./sample.dat', dtype='>f', count=4500*24, offset=4500*24*skip)\
+           .reshape(24,5,30,30)
+  return data[0,0]
